9b9f74a4e3c1f85bc3b03e28ba4ac9d86c688df9
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:32:15 +0300

    Add files via upload

---
+++ 1.24/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1._24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, x;
+
+            Console.WriteLine("Первое число: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Второе число: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            a = Math.Sqrt((2 * +Math.Sin(3 * a)) / 3.56);
+            x = 3.2 + Math.Sqrt(1 + x) / (5 * x);
+
+            Console.WriteLine($"Результат а) = {a}  ");
+            Console.WriteLine($"Результат х) = {x}  ");
+        }
+    }
+
+}
